eef5c2a1b3871c59876ec906ebfd89fea2a25c2d
nbhaskareddy <bhaskar.nic.in@gmail.com>
2024-12-27 11:09:01 +0530

    initial setup done

---
+++ AppTechTests/AppTechTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppTechTests.swift
+//  AppTechTests
+//
+//  Created by N Bhaskar Reddy on 27/12/24.
+//
+
+import Testing
+@testable import AppTech
+
+struct AppTechTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
